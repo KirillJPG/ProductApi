93c00e088d81b8c11e83100e3be6b436e38e349c
kirillllllcmirnov <kiryusha.smirnov@internet.ru>
2024-12-03 20:01:18 +0300

    start project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vitePluginSvgr from 'vite-plugin-svgr'
+import path from 'path'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),vitePluginSvgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@model': path.resolve(__dirname, './src/model'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@constant': path.resolve(__dirname, './src/constant'),
+    },
+  },
+  
+})
